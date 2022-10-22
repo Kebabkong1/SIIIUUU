1bf43c10c0df8b774c25e6ee2e8e35b135f5a0a8
Kebabkong1 <sondre-relling@hotmail.com>
2022-10-22 13:56:54 +0200

    added structure and import constants, fixed request call

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from asyncio import constants
 import requests
-import common/constants
+from common.constants import leagueID
 headers = {"apikey": "YOUR-APIKEY"}
 x = requests.get('http://api.football-data.org/v4/competitions/', headers=headers)
 
 print(x.text)
-print(constants-)
+print(leagueID)
