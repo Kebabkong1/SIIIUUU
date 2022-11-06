7847f9dd2db7858ad6147a608184349336b999e2
sondre kebab <sondre-relling@hotmail.com>
2022-11-06 17:11:15 +0100

    added result array, fixed env secret, fixed unicode chars

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
 from dataclasses import dataclass
 import requests
 import json
+import time
 from common.constants import league_id
 from env.secrets import api_token
 
-path = "SIIIUUU\common\league-data.json"
+path = 'leagues.json'
 headers = {"X-Auth-Token": api_token}
-#response = requests.get('https://api.football-data.org/v4/competitions/PL/matches?status=FINISHED', headers=headers)
+league_file = open(path, "r",  encoding='utf8')
+league_data = json.load(league_file)
 
-#response_json = response.json()
+def played_matches_response(league_short_name):
+    response = requests.get(f'http://api.football-data.org/v4/competitions/{league_short_name}/matches?status=FINISHED', headers=headers)
+    print(response.status_code)
+    return response
 
-#print(response_json["matches"][0]["score"])
-#print(response_json["matches"][0]["homeTeam"]["name"])
-#print(response_json["matches"][0]["awayTeam"]["name"])
-#print(response_json)
+def add_result_array(matches, league_short_name):
+    for index, match_info in enumerate(matches):
+        home_team = matches[index]["homeTeam"]["name"]
+        away_team = matches[index]["awayTeam"]["name"]
+        result = match_info["score"]["winner"]
+        match result:
+            case 'HOME_TEAM':
+                league_data["leagues"][league_short_name][home_team].setdefault('resultArray', []).append('3')
+                league_data["leagues"][league_short_name][away_team].setdefault('resultArray', []).append('0')
+            case 'AWAY_TEAM':
+                league_data["leagues"][league_short_name][home_team].setdefault('resultArray', []).append('0')
+                league_data["leagues"][league_short_name][away_team].setdefault('resultArray', []).append('3')        
+            case 'DRAW':
+                league_data["leagues"][league_short_name][home_team].setdefault('resultArray', []).append('1')
+                league_data["leagues"][league_short_name][away_team].setdefault('resultArray', []).append('1')        
+            case _: 
+                print('undefined value in score winner :'  + str(result))
 
-f = open(path, "r")
-data = json.load(f)
-league_names= ["PL"]
+for index, league_short_name in enumerate(league_id.values()):
+    print(league_short_name)
+    res = played_matches_response(league_short_name)
+    res_json = res.json()
+    matches = res_json['matches']
+    add_result_array(matches, league_short_name)
+    if (index==5):
+        time.sleep(61)
 
+with open("leagues_result.json", "w",  encoding='utf8') as outfile:
+    json.dump(league_data, outfile, ensure_ascii=False)
 
-for leagues in data["leagues"]:
-    for league in leagues:
-        print(league)
-        for team in leagues[league]:
-            print(team)
-
-f.close()
-
-
-print(data["leagues"][0]["PL"][0])
-data["leagues"][0]["PL"][0]["Arsenal"]["kim"] = 1
-
-fw = open(path, 'w')
-
-fw.write(json.dumps(data))
-
-fw.close
